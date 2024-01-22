5a31e1a3719dfdc81296b0db13abe30023e6dee3
Leonelsk8 <leonelgomez.sk8@gmail.com>
2024-01-21 22:40:51 -0300

    first commit

---
+++ nextjs-mysql-crud/src/app/about/page.jsx
@@ -0,0 +1,44 @@
+function AboutPage() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-2xl font-bold my4">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        reiciendis voluptatem aliquid possimus distinctio maxime illum quod
+        alias consequatur et ipsa eligendi soluta neque quae perferendis hic,
+        aperiam praesentium quidem. Sequi repudiandae quam ducimus laboriosam
+        nobis ut provident deserunt vel amet voluptatibus blanditiis nihil nemo
+        totam quas, a error corrupti corporis dolorum. Debitis dolor repellendus
+        mollitia nostrum! Accusantium consectetur suscipit rem culpa error enim
+        cum possimus mollitia voluptates? Odio amet magni, quasi sint a omnis
+        veniam natus quae dolor numquam ut ex blanditiis eaque sequi architecto
+        perspiciatis suscipit. Nobis praesentium cum nesciunt a quae esse
+        dolorum incidunt dignissimos quam architecto, doloribus iusto, aliquid,
+        maiores impedit fugit nulla veritatis earum excepturi delectus itaque
+        modi adipisci reiciendis? Dicta dolores obcaecati, blanditiis itaque
+        explicabo esse soluta consequuntur provident debitis reprehenderit.
+        Expedita autem, aliquid at veritatis temporibus hic eaque id ut placeat
+        reiciendis nostrum totam odit ratione assumenda, tempora fuga quas
+        officia unde provident sunt vel tempore necessitatibus excepturi
+        suscipit? Totam, quaerat illum aliquid nulla ab alias eos perspiciatis
+        aperiam dignissimos neque dolores illo aut distinctio eius commodi
+        excepturi rerum quis error ipsam harum. Corporis, omnis tempore ipsam
+        mollitia voluptatem id dignissimos iusto. Quidem veritatis nobis culpa
+        quae, alias, ipsam assumenda quod molestias velit officiis ducimus,
+        adipisci illum quam enim magni voluptatibus expedita repudiandae facilis
+        quas sunt harum modi. Odit inventore eos nobis enim maiores. Esse atque
+        voluptatum explicabo debitis, modi autem illum repellat vitae quaerat
+        laboriosam accusantium quidem eligendi distinctio animi corrupti unde
+        fuga maxime exercitationem ipsa officia vero praesentium ipsum amet
+        itaque! Ea cum nam laborum. Accusantium dicta expedita iusto impedit,
+        architecto placeat ea, enim quidem aliquid soluta ipsum, cumque saepe ut
+        odio! Accusantium nemo sapiente totam, eveniet atque magnam iusto
+        obcaecati minima cumque consectetur non libero tempora, neque rem odio
+        enim quis. Dolorum quis veritatis in doloribus dolores quaerat quidem
+        consectetur.
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
